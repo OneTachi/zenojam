a9d034d35fcd656d6d28fd41cdde2ec001e08dea
OneTachi <n2verz@gmail.com>
2021-12-18 13:00:12 -0500

    basic jump

--- player/Player.gd
+++ player/Player.gd
@@ -3,20 +3,35 @@ extends KinematicBody2D
 var moveIncr = .2
 var xMove = 0
 var yMove = 0
+var jumped = false
+var jump_position
 
 func _ready():
 	pass
 
 func _physics_process(delta):
 	var xRaw = Input.get_action_strength("right") - Input.get_action_strength("left")
-	var yRaw = Input.get_action_strength("jump")
 	
 	if xRaw != 0:
 		xMove = lerp(xMove, xRaw * 350, moveIncr)
 	else:
 		xMove = lerp(xMove, 0, .5)
 	
-	yMove += 10
+	if !jumped && Input.is_action_pressed("jump"): 
+		if !yMove <= 0:
+			jump_position = self.position.y
+		yMove = -1000
+	
+	if !is_on_floor() && yMove < 0 && self.position.y - jump_position < -200:
+		jumped = true
+	elif is_on_floor():
+		jumped = false
+	
+	if jump_position != null:
+		print(self.position.y - jump_position )
+	yMove = min(yMove + 50, 1000)
+	if jumped && yMove > 0 && yMove < 1000: 
+		yMove = yMove * (1 + .05) 
 	
 	var movement = Vector2(xMove, yMove)
-	move_and_slide(movement)
+	move_and_slide(movement, Vector2.UP)
